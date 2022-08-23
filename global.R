1a97020052de29434cd26db27de588cb7e6214ce
eliansoutu <eliansoutul@gmail.com>
2022-08-23 12:11:22 -0300

    actualizo app con capas rn y collapsible panels

--- global.R
+++ global.R
@@ -8,12 +8,15 @@ library(shinyvalidate) # Input Validation for Shiny Apps, CRAN v0.1.2
 library(shinyWidgets) # Custom Inputs Widgets for Shiny, CRAN v0.7.0
 library(colourpicker) # A Colour Picker Tool for Shiny and for Selecting Colours in Plots, CRAN v1.1.1
 library(waiter) # Loading screens for Shiny
+library(shinyBS) # Twitter Bootstrap Components for Shiny, CRAN v0.61.1
 
 # Cargo mapa de Argentina
 mapa_arg <- get_geo("ARGENTINA", level = "provincia") %>% 
   add_geo_codes() %>% 
   mutate(name_iso = case_when(name_iso == "Ciudad Autónoma de Buenos Aires" ~ "CABA",
                               TRUE ~ name_iso))
+
+rutas_naturales_base <- st_read("/srv/DataDNMYE/capas_sig/rutas_naturales.gpkg", "rutas_naturales")
 
 # Cargo módulo de capas
 source("modules/add_layer.R", encoding = "UTF-8")
--- modules/add_layer.R
+++ modules/add_layer.R
@@ -144,7 +144,7 @@ capasServer <- function(id) {
     
     observeEvent(list(input$opcionSize, input$vector),{
       if (input$opcionSize == "Único") {
-        updateSelectInput(inputId = "size", choices = c(0.1,0.5,1:7))
+        updateSelectInput(inputId = "size", choices = c(0.1,0.5,1:7), selected = 3)
       } else if (input$opcionSize == "Según variable") {
         updateSelectInput(inputId = "size", choices = colnames(capa() %>% select_if(is.numeric) %>% st_set_geometry(NULL)))
       }
@@ -268,19 +268,19 @@ capasServer <- function(id) {
     })
     
     # Control de botón para agregar capa
-    click <- eventReactive(input$btnCapa, {
-      if(input$btnCapa){
-        "Click"
-      } else {
-        "No click"
-      }
-    }, ignoreNULL = FALSE)
+    # click <- eventReactive(input$btnCapa, {
+    #   if(input$btnCapa){
+    #     "Click"
+    #   } else {
+    #     "No click"
+    #   }
+    # }, ignoreNULL = FALSE)
     
 
     
     # Envío capa y valores al server
     return(list(a = reactive(layer$capa()),
-                b = reactive(click()),
+                #b = reactive(click()),
                 c = reactive(refs()),
                 d = reactive(input$file)
                 ))
